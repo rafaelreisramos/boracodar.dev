caef66652cfd29755d68e2c4c03c6c649a6a94f7
rafaelreisramos <rrramos@gmail.com>
2023-02-06 14:13:09 -0300

    fix: img src path

--- desafio-02_product-card/script.js
+++ desafio-02_product-card/script.js
@@ -3,13 +3,13 @@ const cardImg = document.querySelector('img')
 const closeIcon = document.querySelector('.close')
 
 rotateIcon.addEventListener('click', () => {
-  cardImg.setAttribute('src', '/assets/images/couch.gif')
+  cardImg.setAttribute('src', './assets/images/couch.gif')
   rotateIcon.style.visibility = 'hidden'
   closeIcon.style.visibility = 'visible'
 })
 
 closeIcon.addEventListener('click', () => {
-  cardImg.setAttribute('src', '/assets/images/couch.png')
+  cardImg.setAttribute('src', './assets/images/couch.png')
   closeIcon.style.visibility = 'hidden'
   rotateIcon.style.visibility = 'visible'
 })
